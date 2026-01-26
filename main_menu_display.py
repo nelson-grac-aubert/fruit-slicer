2adac1db7ed7ad7bdf5a9d5f0a6e25becfda9b29
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-01-26 21:19:30 +0100

    feat : add more fruits

--- main_menu_display.py
+++ main_menu_display.py
@@ -43,13 +43,15 @@ def draw_all_fruits(screen):
     screen_h = screen.get_height()
 
     fruits = [
-        ("assets/images/raspberry.png",  (screen_w * 0.15, screen_h * 0.25)),
-        ("assets/images/blackberry.png", (screen_w * 0.85, screen_h * 0.30)),
-        ("assets/images/watermelon.png", (screen_w * 0.40, screen_h * 0.80)),
-        ("assets/images/banana.png",     (screen_w * 0.10, screen_h * 0.55)),
-        ("assets/images/orange.png",     (screen_w * 0.90, screen_h * 0.70)),
-        ("assets/images/bomb.png",       (screen_w * 0.70, screen_h * 0.70)),
-        ("assets/images/ice.png",        (screen_w * 0.50, screen_h * 0.45)),
+        ("assets/images/raspberry.png",             (screen_w * 0.15, screen_h * 0.25)),
+        ("assets/images/blackberry.png",            (screen_w * 0.85, screen_h * 0.30)),
+        ("assets/images/big_watermelon.png",        (screen_w * 0.40, screen_h * 0.80)),
+        ("assets/images/banana.png",                (screen_w * 0.10, screen_h * 0.55)),
+        ("assets/images/orange.png",                (screen_w * 0.90, screen_h * 0.70)),
+        ("assets/images/bomb.png",                  (screen_w * 0.70, screen_h * 0.70)),
+        ("assets/images/ice.png",                   (screen_w * 0.50, screen_h * 0.45)),
+        ("assets/images/big_strawberry.png",        (screen_w * 0.50, screen_h * 0.85)),
+
     ]
 
     for path, pos in fruits:
