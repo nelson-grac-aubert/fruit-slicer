1df4b9cde2b3fd5206d36418799b829686f8c229
Yannisand <yannis.sandoval22@gmail.com>
2026-01-27 11:58:31 +0100

    Add difficulty settings

--- Programy.py
+++ Programy.py
@@ -3,25 +3,28 @@ import time
 
 Inputlist1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 Inputlist2 = Inputlist1 + ['0','1','2','3','4','5','6','7','8','9','²']
-Inputlist3 = Inputlist2 + ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-Inputlist4 = Inputlist3 + ['&','é','"',"'",'(','-','è','_','ç','à',')','=','^','$','ù','*','<','>',',',';',':','!','°','+','¨','£','%','µ','?','.','/','§']
-Inputlist5 = Inputlist4 + ['~','#','{','[','|','`','\\','^','@',']','}','¤','€']
+Inputlist3 = Inputlist2 + ['&','é','"',"'",'(','-','è','_','ç','à',')','=','^','$','ù','*','<',',',';',':','!']
+
+Easysetting = (Inputlist1,5,10,(1,3),(8,14))
+Normalsetting = (Inputlist2,10,5,(3,5),(12,18))
+Hardsetting = (Inputlist3,25,1,(5,7),(16,22))
 
 def callobjects(currentobject,timer):
     print(currentobject,timer)
 
 
-def setobject(inputlist):
+def setobject(Difficulty):
     timer = 1
-    percentbomb = 1000
-    percentice = 500
+    inputlist = Difficulty[0]
+    percentbomb = Difficulty[1]
+    percentice = Difficulty[2]
     inuseinput = []
     for loop in range(1000):
         currentobject = []
-        objecttype = random.randint(1,10000)
+        objecttype = random.randint(1,100)
         if objecttype <= percentbomb:
             currentobject += ["Bomb"]
-        elif 10000 - percentice < objecttype <+ 10000:
+        elif 100 - percentice < objecttype <= 100:
             currentobject += ["Ice"]
         else:
             currentobject += ["Fruit"]
@@ -36,4 +39,4 @@ def setobject(inputlist):
         if not timer <= 0.2:
             timer -= 0.005
 
-setobject(Inputlist4)
+setobject(Easysetting)
